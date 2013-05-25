4e7ac24563e61a1603d3a51ef759166fe212e69f
David Moore <dmoo1790@ihug.co.nz>
2013-05-25 21:05:09 +1200

    try again

---
+++ sosgame1/src/com/example/sosgame1/Board.java
@@ -0,0 +1,11 @@
+package com.example.sosgame1;
+
+public class Board {
+
+	private MyGLRenderer renderer = null;
+	
+	public Board(MyGLRenderer renderer) {
+		this.renderer = renderer;
+	}
+	
+}
---
+++ sosgame1/src/com/example/sosgame1/Tile.java
@@ -0,0 +1,39 @@
+package com.example.sosgame1;
+
+public class Tile extends Cube {
+
+    private char letter; //either "S" or "O"
+    private Player player;
+    
+	public Tile(MyGLRenderer renderer, float x, float y) {
+		super(renderer, x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Tile(MyGLRenderer renderer) {
+		super(renderer);
+		// TODO Auto-generated constructor stub
+	}
+
+	public char getLetter() {
+        return letter;
+    }
+
+    public void setLetter(char letter) {
+    	this.letter = letter;
+    	if (letter == 'O') {
+    		rotationY = 180;
+    	} else {
+    		rotationY = 0;
+    	}
+    }
+    
+    public Player getPlayer() {
+            return player;
+    }
+    
+    public void setPlayer(Player player) {
+            this.player = player;
+    }
+
+}
