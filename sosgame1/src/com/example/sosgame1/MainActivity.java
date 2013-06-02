1c0f47d994dde3425d42cdcf4e11e1a02fd28b16
Bea Batanes <beabatanes@gmail.com>
2013-06-03 09:41:36 +1200

    button animation
    Added simple animation on splash page buttons.

--- sosgame1/src/com/example/sosgame1/MainActivity.java
+++ sosgame1/src/com/example/sosgame1/MainActivity.java
@@ -18,9 +18,14 @@
 
 package com.example.sosgame1;
 
+import splash.Splash;
+
 import com.example.sosgame1.MyGLSurfaceView;
 import com.example.sosgame1.controller.LogicControl;
 import android.os.Bundle;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -29,6 +34,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 
@@ -43,12 +49,15 @@ public class MainActivity extends Activity implements OnClickListener,
     private boolean isPanningX = false;
     private boolean isPanningY = false;
     private LogicControl controller = null;
+    
+//    private Splash splash = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// Create an instance of the logic controller
 		controller = new LogicControl();
+//		splash = new Splash();
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 		mainView = (RelativeLayout) findViewById(R.id.rlMain);
@@ -67,7 +76,9 @@ public class MainActivity extends Activity implements OnClickListener,
 	}
 
 	public void onClick(View v) {
+		
 		LayoutInflater inflater = getLayoutInflater();
+		
 		switch (v.getId()) {
 		case R.id.btnView:
 			viewAdjustView = inflater.inflate(R.layout.view_adjust, null);
@@ -109,7 +120,36 @@ public class MainActivity extends Activity implements OnClickListener,
 			viewAdjustView = inflater.inflate(R.layout.splash_page, null); /////BEA
 			if (viewAdjustView != null) {
 				mainView.addView(viewAdjustView);
+		        ((ImageButton) findViewById(R.id.btnPlay)).setOnClickListener(this);
+		        ((ImageButton) findViewById(R.id.btnSettings)).setOnClickListener(this);
 			}
+
+			break;
+		case R.id.btnPlay:
+			ImageButton buttonPlay = (ImageButton) findViewById(R.id.btnPlay); //can't put before switch it will be null
+//			// set value for rotating
+//			angleRotation = 180;
+//	
+//			// reset values after 360°
+//			if (buttonPlay.getRotation() == 180)
+//				angleRotation = 360;
+//			else if (buttonPlay.getRotation() == 360)
+//				buttonPlay.setRotation(0);
+//	
+//			// create object animator for rotating and set duration
+//			ObjectAnimator rotateAni = ObjectAnimator.ofFloat(buttonPlay,
+//					"rotation", angleRotation);
+//			rotateAni.setDuration(2000);
+//			rotateAni.start();
+			AnimatorSet btnPlayAniSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.button_rotate); //can't reuse
+			btnPlayAniSet.setTarget(buttonPlay);
+			btnPlayAniSet.start();
+			break;
+		case R.id.btnSettings:
+			ImageButton buttonSettings = (ImageButton) findViewById(R.id.btnSettings);
+			AnimatorSet btnSettingsAniSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.button_rotate);
+			btnSettingsAniSet.setTarget(buttonSettings);
+			btnSettingsAniSet.start();
 			break;
 		}
 	}
--- sosgame1/src/splash/Splash.java
+++ sosgame1/src/splash/Splash.java
@@ -8,39 +8,19 @@ import android.graphics.BitmapFactory;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class Splash extends SurfaceView implements SurfaceHolder.Callback{
+public class Splash {
 
 	private Bitmap test;
 
 	private Context context;
 	
-	public Splash(Context context) {
-		super(context);
+	public Splash() {
 		this.context = context;
 		
 		test = BitmapFactory.decodeResource(context.getResources(), R.drawable.test);
-		
-		SurfaceHolder holder = getHolder();
-		holder.addCallback(this);	
+
 	}
 
-	@Override
-	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void surfaceCreated(SurfaceHolder holder) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void surfaceDestroyed(SurfaceHolder holder) {
-		// TODO Auto-generated method stub
-		
-	}
 
 
 
