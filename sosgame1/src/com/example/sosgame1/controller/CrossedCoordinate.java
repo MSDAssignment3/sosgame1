e3a648da14fa6d281068c97a023ca21aa2c9550c
Ar Kar Jue <Ar Kar Jue@172.25.28.217>
2013-06-20 15:49:26 +1200

    Logic Update

---
+++ sosgame1/src/com/example/sosgame1/controller/CrossedCoordinate.java
@@ -0,0 +1,74 @@
+package com.example.sosgame1.controller;
+
+//This class will store the crossed coordinate value of cells where line has been made
+public class CrossedCoordinate {
+
+	private int i1;
+	private int j1;
+	private int i2;
+	private int j2;
+	private int i3;
+	private int j3;
+	
+	public CrossedCoordinate(){};
+	
+	public CrossedCoordinate(int i1,int j1,int i2,int j2,int i3,int j3)
+	{
+	   this.i1 = i1;
+	   this.j1 = j1;
+	   this.i2 = i2;
+	   this.j2 = j2;
+	   this.i3 = i3;
+	   this.j3 = j3;		
+	}
+	
+	
+	public int getI1() {
+		return i1;
+	}
+
+	public void setI1(int i1) {
+		this.i1 = i1;
+	}
+
+	public int getJ1() {
+		return j1;
+	}
+
+	public void setJ1(int j1) {
+		this.j1 = j1;
+	}
+
+	public int getI2() {
+		return i2;
+	}
+
+	public void setI2(int i2) {
+		this.i2 = i2;
+	}
+
+	public int getJ2() {
+		return j2;
+	}
+
+	public void setJ2(int j2) {
+		this.j2 = j2;
+	}
+
+	public int getI3() {
+		return i3;
+	}
+
+	public void setI3(int i3) {
+		this.i3 = i3;
+	}
+
+	public int getJ3() {
+		return j3;
+	}
+
+	public void setJ3(int j3) {
+		this.j3 = j3;
+	}
+	
+}
--- sosgame1/src/com/example/sosgame1/controller/LogicControl.java
+++ sosgame1/src/com/example/sosgame1/controller/LogicControl.java
@@ -1,18 +1,14 @@
 package com.example.sosgame1.controller;
 
+
 import java.io.BufferedReader;
-import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-import android.graphics.Point;
-import android.graphics.PointF;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.example.sosgame1.Board;
-import com.example.sosgame1.Cube;
-import com.example.sosgame1.Line;
 import com.example.sosgame1.Player;
-import com.example.sosgame1.Tile;
 
 //Class for game logic
 public class LogicControl {
@@ -23,17 +19,32 @@ public class LogicControl {
 	private int totalCorrect = 0;
 	//total will be 
 	private String input [][] = new String[5][5];	
-	private Board board = null;
 	
-	public int currentPlayerColour = Player.COLOUR_BLUE;
+	private boolean firstPlayer = false;  
+	private boolean secondPlayer = true;
+	private boolean giveTurnToFirst = false;
+	private boolean giveTurnToSecond = false;	
+	private int firstPlayerScore = 0;
+	private int secondPlayerScore = 0;
+	private List <CrossedCoordinate> crossedCoordinateList = new ArrayList<CrossedCoordinate>();
+	public int currentPlayerColour = Player.COLOUR_BLUE;  //blue for player 1 and red for player 2
+	//public int secondPlayerColour = Player.COLOUR_RED;
+	private Board board = null;
 	
 	public LogicControl()
 	{
 		
 	}  
 	
-	public LogicControl(Board board) {
+	public LogicControl(Board board)
+	{
 		this.board = board;
+	}
+	
+	//TODO need to modify
+	public void getPlayerNames()
+	{
+		
 	}
 	
 	public void readCheck()
@@ -63,8 +74,30 @@ public class LogicControl {
 	public void getAndCheck(int indexRow,int indexColumn,String inputValue)
 	{
 		 input[indexRow][indexColumn] =inputValue;
+		 
+		 //toggling boolean value
+		 if(firstPlayer==true && giveTurnToFirst==false)
+		 {
+			 secondPlayer = true;
+			 firstPlayer = false;
+			 currentPlayerColour = Player.COLOUR_RED;
+		 }
+		 else if(secondPlayer == true && giveTurnToSecond ==false)
+		 {
+			 firstPlayer = true;
+			 secondPlayer = false;
+			 currentPlayerColour = Player.COLOUR_BLUE;
+		 }
+		 
+		 giveTurnToFirst = false; //reset
+		 giveTurnToSecond = false; //reset
+		 
 		 checkHorizontally();
 		 checkVertically();
+		 checkTopRight();
+		 checkTopLeft();
+		 checkBottomLeft();
+		 checkBottomRight();
 	}
 	
 	/**
@@ -100,18 +133,26 @@ public class LogicControl {
 						answer.trim();						
 						if(answer.equalsIgnoreCase("SOS"))
 						{		
-							totalCorrect++;
-							System.out.println(answer+" Horizontally");
-							System.out.println("Total correct "+totalCorrect);
-							if (board != null) {
-								if (!board.lineAlreadyAdded(i, k - 2, i, k)) {
-									board.addLine(i, k - 2, i, k, currentPlayerColour);
-								}
+							//get coordinates
+							int temI1 = i;
+							int temJ1 = k-2;
+							int temI2 = i;
+							int temJ2 = k-1;
+							int temI3 = i;
+							int temJ3 = k;
+							boolean hasStored = checkIfStored(temI1,temJ1,temI2,temJ2,temI3,temJ3);						
+							
+							if(hasStored==false)
+							{
+								totalCorrect++;
+								changePlayerTurn();							
+								board.addLine(temI1, temJ1, temI3, temJ3 , currentPlayerColour);
+								System.out.println(answer+" Horizontally");
+								System.out.println("Total correct "+totalCorrect);									
+								
+								//store coordinates							
+								storeCoordinates(temI1,temJ1,temI2,temJ2,temI3,temJ3);								
 							}
-						}
-						else
-						{
-							//System.out.println("Incorrect0");
 						}		
 						
 						count = 0; //reset counts	to 0					
@@ -133,20 +174,27 @@ public class LogicControl {
 						answer.trim();						
 						if(answer.equalsIgnoreCase("SOS"))
 						{		
-							totalCorrect++;
-							System.out.println(answer+" Horizontally");
-							System.out.println("Total correct "+totalCorrect);										
-							if (board != null) {
-								if (!board.lineAlreadyAdded(i, k - 2, i, k)) {
-									board.addLine(i, k - 2, i, k, currentPlayerColour);
-								}
-							}
+							//get coordinates
+							int temI1 = i;
+							int temJ1 = k-2;
+							int temI2 = i;
+							int temJ2 = k-1;
+							int temI3 = i;
+							int temJ3 = k;
+							boolean hasStored = checkIfStored(temI1,temJ1,temI2,temJ2,temI3,temJ3);	
+							
+							if(hasStored==false)
+							{
+								totalCorrect++;
+								changePlayerTurn();		
+								System.out.println(answer+" Horizontally");
+								System.out.println("Total correct "+totalCorrect);										
+								board.addLine(temI1, temJ1, temI3, temJ3 , currentPlayerColour);
+								//store coordinates							
+								storeCoordinates(temI1,temJ1,temI2,temJ2,temI3,temJ3);
+							}							
+							
 						}
-						else
-						{
-							//System.out.println("Incorrect1");
-						}	
-						
 						count = 0; //reset counts	to 0					
 						answer = "";	
 					}
@@ -165,20 +213,31 @@ public class LogicControl {
 					{					
 						answer.trim();						
 						if(answer.equalsIgnoreCase("SOS"))
-						{		
-							totalCorrect++;
-							System.out.println(answer+" Horizontally");
-							System.out.println("Total correct "+totalCorrect);										
-							if (board != null) {
-								if (!board.lineAlreadyAdded(i, k - 2, i, k)) {
-									board.addLine(i, k - 2, i, k, currentPlayerColour);
-								}
+						{	
+							//get coordinates
+							int temI1 = i;
+							int temJ1 = k-2;
+							int temI2 = i;
+							int temJ2 = k-1;
+							int temI3 = i;
+							int temJ3 = k;
+							
+							boolean hasStored = checkIfStored(temI1,temJ1,temI2,temJ2,temI3,temJ3);		
+							
+							System.out.println(hasStored+"case2");
+							if(hasStored==false)
+							{
+								totalCorrect++;
+								changePlayerTurn();	
+								board.addLine(temI1, temJ1, temI3, temJ3 , currentPlayerColour);
+								System.out.println(answer+" Horizontally");
+								System.out.println("Total correct "+totalCorrect);							
+	
+								//store coordinates							
+								storeCoordinates(temI1,temJ1,temI2,temJ2,temI3,temJ3);	
 							}
 						}
-						else
-						{
-							//System.out.println("Incorrect2");
-						}	
+						
 						
 						count = 0; //reset counts	to 0					
 						answer = "";	
@@ -227,19 +286,29 @@ public class LogicControl {
 						answer.trim();						
 						if(answer.equalsIgnoreCase("SOS"))
 						{		
-							totalCorrect++;
-							System.out.println(answer+" Vertically");
-							System.out.println("Total correct "+totalCorrect);											
-							if (board != null) {
-								if (!board.lineAlreadyAdded(k - 2, j, k, j)) {
-									board.addLine(k - 2, j, k, j, currentPlayerColour);
-								}
+							//get coordinates
+							int temI1 = k-2;
+							int temJ1 = j;
+							int temI2 = k-1;
+							int temJ2 = j;
+							int temI3 = k;
+							int temJ3 = j;
+							boolean hasStored = checkIfStored(temI1,temJ1,temI2,temJ2,temI3,temJ3);
+							
+							if(hasStored==false)
+							{
+								totalCorrect++;
+								changePlayerTurn();
+								board.addLine(temI1, temJ1, temI3, temJ3 , currentPlayerColour);
+								System.out.println(answer+" Vertically");
+								System.out.println("Total correct "+totalCorrect);		
+								
+								//store coordinates							
+								storeCoordinates(temI1,temJ1,temI2,temJ2,temI3,temJ3);
+
 							}
 						}
-						else
-						{
-							//System.out.println("Incorrect0");
-						}		
+								
 						
 						count = 0; //reset counts	to 0					
 						answer = "";
@@ -259,19 +328,28 @@ public class LogicControl {
 					{					
 						answer.trim();						
 						if(answer.equalsIgnoreCase("SOS"))
-						{		
-							totalCorrect++;
-							System.out.println(answer+" Vertically");
-							System.out.println("Total correct "+totalCorrect);										
-							if (board != null) {
-								if (!board.lineAlreadyAdded(k - 2, j, k, j)) {
-									board.addLine(k - 2, j, k, j, currentPlayerColour);
-								}
+						{	
+							//get coordinates
+							int temI1 = k-2;
+							int temJ1 = j;
+							int temI2 = k-1;
+							int temJ2 = j;
+							int temI3 = k;
+							int temJ3 = j;
+							boolean hasStored = checkIfStored(temI1,temJ1,temI2,temJ2,temI3,temJ3);
+							
+							if(hasStored==false)
+							{
+								totalCorrect++;
+								changePlayerTurn();
+								board.addLine(temI1, temJ1, temI3, temJ3 , currentPlayerColour);
+								System.out.println(answer+" Vertically");
+								System.out.println("Total correct "+totalCorrect);	
+								
+								//store coordinates							
+								storeCoordinates(temI1,temJ1,temI2,temJ2,temI3,temJ3);
+
 							}
-						}
-						else
-						{
-							//System.out.println("Incorrect1");
 						}	
 						
 						count = 0; //reset counts	to 0					
@@ -293,19 +371,26 @@ public class LogicControl {
 						answer.trim();						
 						if(answer.equalsIgnoreCase("SOS"))
 						{		
-							totalCorrect++;
-							System.out.println(answer+" Vertically");
-							System.out.println("Total correct "+totalCorrect);										
-							if (board != null) {
-								if (!board.lineAlreadyAdded(k - 2, j, k, j)) {
-									board.addLine(k - 2, j, k, j, currentPlayerColour);
-								}
+							//get coordinates
+							int temI1 = k-2;
+							int temJ1 = j;
+							int temI2 = k-1;
+							int temJ2 = j;
+							int temI3 = k;
+							int temJ3 = j;
+							boolean hasStored = checkIfStored(temI1,temJ1,temI2,temJ2,temI3,temJ3);
+							
+							if(hasStored==false)
+							{							
+								totalCorrect++;
+								changePlayerTurn();
+								System.out.println(answer+" Vertically");
+								System.out.println("Total correct "+totalCorrect);			
+								board.addLine(temI1, temJ1, temI3, temJ3 , currentPlayerColour);
+								//store coordinates							
+								storeCoordinates(temI1,temJ1,temI2,temJ2,temI3,temJ3);
 							}
 						}
-						else
-						{
-							//System.out.println("Incorrect2");
-						}	
 						
 						count = 0; //reset counts	to 0					
 						answer = "";	
@@ -321,6 +406,383 @@ public class LogicControl {
 				
 	}
 	
+	/**
+	 * Checking from top right
+	 */
+	public void checkTopRight()
+	{
+		//j value will start from existing columns -2 (array start from 0)
+		int i=0,j=2;
+		int columns = 2; //start from 2 and will increment till it is  equal to 4 
+		int count =0;
+		String answer = "";	
+		
+		while(j!=-1)
+		{						
+				// k=j and k is also used as i
+				for(int k=0;k<=columns;k++)
+				{
+					if(input[k][j+k]!=null)
+					{
+						answer += input[k][j+k].toString();	
+					}
+					count++;
+					if(count==3)
+					{
+						answer.trim();						
+						if(answer.equalsIgnoreCase("SOS"))
+						{	
+							//get coordinates
+							int temI1 = k-2;
+							int temJ1 = (j+k)-2;
+							int temI2 =k-1;
+							int temJ2 = (j+k)-1;
+							int temI3 = k;
+							int temJ3 = j+k;
+							boolean hasStored = checkIfStored(temI1,temJ1,temI2,temJ2,temI3,temJ3);
+							
+							if(hasStored==false)
+							{														
+								totalCorrect++;
+								changePlayerTurn();	
+								System.out.println(answer+"Top Right");
+								System.out.println("Total correct "+totalCorrect);	
+								board.addLine(temI1, temJ1, temI3, temJ3 , currentPlayerColour);
+								//store coordinates							
+								storeCoordinates(temI1,temJ1,temI2,temJ2,temI3,temJ3);
+
+							}
+						}
+						
+						if(k<columns)//if there are more cells to check then start from previous cell
+						{
+							//System.out.println(k+","+columns+"less than columns");
+							k=k-2;
+						}
+						
+						//reset
+						answer = "";
+						count = 0;
+					}
+	
+				}
+			
+			
+			//reset
+			answer="";
+			count=0;
+			if(columns<4)
+			{
+				columns++;
+			}
+			j--;  //start from one less column			
+			
+		}
+			
+	}
+	
+	
+	/**
+	 * Checking from top right
+	 */
+	public void checkTopLeft()
+	{
+		//j value will start from existing columns -2 (array start from 0)
+		int j=2;
+		int columns = 2; //start from 2 and will increment till it is  equal to 4 
+		int count =0;
+		String answer = "";	
+		
+		while(j<5)
+		{						
+				// k=j 
+				for(int k=columns,i=0;k>=0;k--,i++)
+				{
+					if(input[i][k]!=null)
+					{
+						answer += input[i][k].toString();	
+						//System.out.println(answer+i+","+k);	
+					}
+					count++;
+					if(count==3)
+					{
+						answer.trim();						
+						if(answer.equalsIgnoreCase("SOS"))
+						{	
+							//get coordinates
+							int temI1 = i-2;
+							int temJ1 = k+2;
+							int temI2 = i-1;
+							int temJ2 = k+1;
+							int temI3 = i;
+							int temJ3 = k;
+							boolean hasStored = checkIfStored(temI1,temJ1,temI2,temJ2,temI3,temJ3);
+							
+							if(hasStored==false)
+							{
+								totalCorrect++;
+								changePlayerTurn();	
+								board.addLine(temI1, temJ1, temI3, temJ3 , currentPlayerColour);
+								System.out.println(answer+"Top Left");
+								System.out.println("Total correct "+totalCorrect);		
+								
+								//store coordinates							
+								storeCoordinates(temI1,temJ1,temI2,temJ2,temI3,temJ3);
+
+							}
+						}
+						
+						//System.out.println(i+","+columns+k);
+						if(k>0)//if there are more cells to check then start from previous cell
+						{
+							//System.out.println(i+","+columns+k+"greater than 0");
+							k=k+2;
+							i=i-2;
+						}
+						
+						//reset
+						answer = "";
+						count = 0;
+					}
+	
+				}
+			
+			
+			//reset
+			answer="";
+			count=0;
+			if(columns<4)
+			{
+				columns++;
+			}
+			j++;  //start from another column			
+			
+		}
+	}
+	
+	
+	/**
+	 * Checking from Bottom left
+	 */
+	public void checkBottomLeft()
+	{
+		//j value will start from zero
+		int columns = 3; //start from existing columns - 1 (array start from zero) 
+		int count =0;
+		String answer = "";	
+		int newI = 1;
+		while(columns>=2)      //j >= than existing columns -2 (array start from zero)
+		{						
+				// j 
+				for(int i=0,j=0;j<=columns;j++,i++)
+				{
+					if(input[i+newI][j]!=null)
+					{
+						answer += input[i+newI][j].toString();	
+						//System.out.println(answer+i+","+k);	
+					}
+					count++;
+					//System.out.println(i+","+columns+j);
+					//System.out.println(count);
+					if(count==3)
+					{
+						answer.trim();						
+						if(answer.equalsIgnoreCase("SOS"))
+						{		
+							//get coordinates
+							int temI1 = (i+newI)-2;
+							int temJ1 = j-2;
+							int temI2 = (i+newI)-1;
+							int temJ2 = j-1;
+							int temI3 = (i+newI);
+							int temJ3 = j;
+							boolean hasStored = checkIfStored(temI1,temJ1,temI2,temJ2,temI3,temJ3);
+							if(hasStored==false)
+							{							
+								totalCorrect++;
+								changePlayerTurn();		
+								board.addLine(temI1, temJ1, temI3, temJ3 , currentPlayerColour);
+								System.out.println(answer+"Bottom Left");
+								System.out.println("Total correct "+totalCorrect);		
+								
+								//store coordinates							
+								storeCoordinates(temI1,temJ1,temI2,temJ2,temI3,temJ3);
+
+							}
+						}
+						
+						//System.out.println(i+","+columns+j);
+						if(j<columns)//if there are more cells to check then start from previous cell
+						{
+							//System.out.println(i+","+columns+j+"less than columns");
+							j=j-2;
+							i=i-2;
+							//System.out.println(i+","+columns+j+"less than columns");
+						}
+						
+						//reset
+						answer = "";
+						count = 0;
+					}
+	
+				}
+			
+			
+			//reset
+			answer="";
+			count=0;
+			columns--; //start from another column	 	
+			newI++;   //i will start from 2
+			
+		}
+	}
+	
+	
+	/**
+	 * Checking from Bottom Right
+	 */
+	public void checkBottomRight()
+	{
+		//j value will start from existing columns value
+		//int j = 4;
+		int columns = 3; //start from existing columns - 1 (array start from zero) 
+		int count =0;
+		String answer = "";	
+		int newI = 1;
+		
+		while(columns>=2)      //j >= than existing columns -2 (array start from zero)
+		{						
+				// j 
+				for(int i=0,j=4;i<=columns;j--,i++)
+				{
+					if(input[i+newI][j]!=null)
+					{
+						answer += input[i+newI][j].toString();	
+						//System.out.println(answer+i+","+k);	
+					}
+					count++;
+					//System.out.println(i+","+columns+j);
+					//System.out.println(count);
+					if(count==3)
+					{
+						answer.trim();						
+						if(answer.equalsIgnoreCase("SOS"))
+						{		
+							//get coordinates
+							int temI1 = (i+newI)-2;
+							int temJ1 = j+2;
+							int temI2 = (i+newI)-1;
+							int temJ2 = j+1;
+							int temI3 = i+newI;
+							int temJ3 = j;
+							
+							boolean hasStored = checkIfStored(temI1,temJ1,temI2,temJ2,temI3,temJ3);
+							if(hasStored==false)
+							{
+								totalCorrect++;
+								changePlayerTurn();		
+								board.addLine(temI1, temJ1, temI3, temJ3 , currentPlayerColour);
+								System.out.println(answer+"Bottom Right");
+								System.out.println("Total correct "+totalCorrect);	
+								
+								//store coordinates							
+								storeCoordinates(temI1,temJ1,temI2,temJ2,temI3,temJ3);
+
+							}
+						}
+						
+						//System.out.println(i+","+columns+j);
+						if(i<columns)//if there are more cells to check then start from previous cell
+						{
+							//System.out.println(i+","+columns+j+"less than columns");
+							j=j+2;
+							i=i-2;
+							//System.out.println(i+","+columns+j+"less than columns");
+						}
+						
+						//reset
+						answer = "";
+						count = 0;
+					}
+	
+				}
+			
+			
+			//reset
+			answer="";
+			count=0;
+			columns--; //start from another column	 	
+			newI++;   //i will start from 2
+			
+		}
+	}
+	
+	/**
+	 * Check if line has been made on these coordinates
+	 * @param i1
+	 * @param j1
+	 * @param i2
+	 * @param j2
+	 * @param i3
+	 * @param j3
+	 * @return
+	 */
+	public boolean checkIfStored(int i1,int j1,int i2,int j2,int i3,int j3)
+	{
+		boolean hasStored = false;
+		int listSize = 0;
+		while(listSize<crossedCoordinateList.size())
+		{
+			CrossedCoordinate crossed = new CrossedCoordinate();
+			crossed=crossedCoordinateList.get(listSize);
+			
+			if(i1==crossed.getI1()&&j1==crossed.getJ1())
+			{
+				System.out.println(crossed.getI1()+""+crossed.getJ1());
+				if( i2==crossed.getI2()&&j2==crossed.getJ2())
+				{
+					System.out.println(crossed.getI2()+""+crossed.getJ2());
+					if( i3==crossed.getI3()&&j3==crossed.getJ3())
+					{
+						System.out.println(crossed.getI3()+""+crossed.getJ3());
+						hasStored = true;
+						break;
+					}
+				}
+			}	
+			listSize++;
+			
+		}
+		return hasStored;
+	}
+	
+	/**
+	 * Toggle player turn and allocate mark
+	 */
+	public void changePlayerTurn()
+	{
+		if(firstPlayer==true)
+		{
+			firstPlayerScore++;
+			giveTurnToFirst =true;
+			System.out.println("FirstPlayer"+firstPlayerScore);
+		}
+		else if(secondPlayer==true)
+		{
+			secondPlayerScore++;
+			giveTurnToSecond = true;
+			System.out.println("SecondPlayer"+secondPlayerScore);
+		}
+	}
+	
+	/**
+	 * Store the coordinates where line is currently made
+	 */
+	public void storeCoordinates(int i1,int j1,int i2,int j2,int i3,int j3)
+	{
+		CrossedCoordinate cro = new CrossedCoordinate(i1,j1,i2,j2,i3,j3);
+		crossedCoordinateList.add(cro);			
+	}
 	
 }
 
